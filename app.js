77703eedf2daba2091397603b410221378ce7405
SR <saodat.riskiyeva@gmail.com>
2025-09-03 11:27:20 -0400

    parsing form data and redirecting requests

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
+
 const app = express();
 
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -27,6 +31,17 @@ app.get("/recommend", function (req, res) {
 app.get("/restaurants", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
   res.sendFile(htmlFilePath);
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+
+  const storedRestaurants = JSON.parse(fileData);
+  storedRestaurants.push(restaurant);
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  res.redirect("/confirm");
 });
 
 app.listen(3000);
