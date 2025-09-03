f89a569a499cb2890c7abaa0ffae82f45700f233
SR <saodat.riskiyeva@gmail.com>
2025-09-02 22:36:54 -0400

    setting up a router for all pages

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World</h1>");
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFilePath);
 });
 
 app.get("/restaurants", function (req, res) {
