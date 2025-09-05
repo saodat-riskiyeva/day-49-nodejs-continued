e130859dd72b5cb3f87f2ba44bab7a739badb121
SR <saodat.riskiyeva@gmail.com>
2025-09-05 18:37:54 -0400

    working with status codes

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get("/restaurants/:id", function (req, res) {
     }
   }
 
-  res.render("404");
+  res.status(404).render("404");
 });
 
 app.get("/recommend", function (req, res) {
@@ -70,11 +70,11 @@ app.post("/recommend", function (req, res) {
 });
 
 app.use(function (req, res) {
-  res.render("404");
+  res.status(400).render("404");
 });
 
 app.use(function (error, req, res, next) {
-  res.render("500");
+  res.status(500).render("500");
 });
 
 app.listen(3000);
